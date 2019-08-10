192e92c21d602106b07de664076406882edef0af
GregoryPrograms <greg.dost@yahoo.com>
2019-08-10 12:04:20 -0700

    Added a rudimentary function to take two inputs and calculate the rock paper scissors winner.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,4 +2,37 @@ function computerplay(){
     rpsArray = ["rock", "paper", "scissors"];
     return rpsArray[Math.floor(Math.random() * rpsArray.length)];
 }
+function roundwin(computerthrow, playerthrow){
+    if(computerthrow == "rock"){
+        switch(playerthrow){
+            case "rock":
+                return 0;
+            case "paper":
+                return 1;
+            case "scissors":
+                return -1;
+        }
+    }
+    else if(computerthrow == "paper"){
+        switch(playerthrow){
+            case "rock":
+                return -1;
+            case "paper":
+                return 0;
+            case "scissors":
+                return 1;
+        }
+    }
+    else if(computerthrow == "scissors"){
+        switch(playerthrow){
+            case "rock":
+                return 1;
+            case "paper":
+                return -1;
+            case "scissors":
+                return 0;
+        }
+
+    }
+}
 console.log(computerplay())
