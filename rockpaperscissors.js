89e00811657722a039396643e1e683a3c168a47c
GregoryPrograms <greg.dost@yahoo.com>
2019-08-17 19:01:22 -0700

    Added game () to actually run through a best of 5 game. Functional RPS now.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,8 +1,8 @@
-function computerplay(){
+function computerPlay(){
     rpsArray = ["rock", "paper", "scissors"];
     return rpsArray[Math.floor(Math.random() * rpsArray.length)];
 }
-function roundwin(computerthrow, playerthrow){
+function playRound(computerthrow, playerthrow){
     if(computerthrow == "rock"){
         switch(playerthrow){
             case "rock":
@@ -35,4 +35,28 @@ function roundwin(computerthrow, playerthrow){
 
     }
 }
-console.log(computerplay())
+function game(){
+    keepGoing = 1
+    while(keepGoing == 1){
+        playerWin = 0;
+        computerWin = 0;
+        playerMessage = "What do you throw: Rock, Paper, or Scissors?";
+        while(playerWin < 3 && computerWin < 3){
+            playerPlay = prompt(playerMessage).toLowerCase();
+            switch (playRound(computerPlay(),playerPlay)){
+                case 1:
+                    playerMessage = "You win this round! The score is: " + ++playerWin + " to " +  computerWin + "\n What do you throw: Rock, Paper, or Scissors?";
+                    break;
+                case 0:
+                    playerMessage = "You tied this round. The score is: " + playerWin + " to " + computerWin + "\n What do you throw: Rock, Paper, or Scissors?"
+                    break;
+                case -1:
+                    playerMessage = "You lost this round. The score is: " + playerWin + " to " + ++computerWin + "\n What do you throw: Rock, Paper, or Scissors?"
+                    break;
+            }
+        }
+        playerMessage = "The game is over. The final score was " + playerWin + " to " + computerWin + "\n Enter 1 to keep playing, or anything else to stop."
+        keepGoing = prompt(playerMessage)
+    }
+}
+game();
